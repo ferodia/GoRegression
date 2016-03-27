4faa7662bafaa26393be8bd3ab64ec60b98ca3ec
Fadwa <fadwa.fathallah@gmail.com>
2016-03-27 21:52:07 +0200

    unit tests for utils

--- utils/algebra.go
+++ utils/algebra.go
@@ -8,18 +8,6 @@ import (
 // This package contains all the mathematical computations needed by the regression algorithms
 
 
-
-
-func DotProduct(X *mat64.Dense, Y *mat64.Vector) *mat64.Dense {
-
-	l, _ := X.Dims()
-	P := mat64.NewDense(l, 1, nil)
-	for i := 0; i < l; i++ {
-		P.Set(i, 0, X.At(i, 0)*Y.At(i, 0))
-	}
-	return P
-}
-
 // Apply simoid function on every element of the matrix
 func SigmoidMatrix(X *mat64.Dense) ( *mat64.Dense) {
 	sigmat := &mat64.Dense{}
@@ -27,12 +15,14 @@ func SigmoidMatrix(X *mat64.Dense) ( *mat64.Dense) {
 	return sigmat
 }
 
+// Subtract same value from all elements of the matrix
 func Subtract(x float64, A * mat64.Dense) (*mat64.Dense)  {
 	S := &mat64.Dense{}
 	S.Apply(func(i, j int, v float64) float64 { return x-v }, A)
 	return S
 }
 
+// Compute the logarithm of each element in the matrix
 func Log(X *mat64.Dense) (*mat64.Dense) {
 	S := &mat64.Dense{}
 	S.Apply(func(i, j int, v float64) float64 { return math.Log(v) }, X)
